ec93cd27e7174046da437db7bb0fc7f86d64b874
shubham <shubhamchauhan@maktech.info>
2020-06-15 06:39:07 +0530

    update final

---
+++ tripplanner/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import Main from "./components/Main";
+import './App.css';
+
+function App() {
+  return (
+    <Main/>
+  );
+}
+
+export default App;
