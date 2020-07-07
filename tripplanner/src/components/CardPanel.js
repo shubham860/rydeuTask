ae65f9f29697dcdf0c7cd9310e1ec339ef83576e
shubham <shubham.chauhan860@gmail.com>
2020-07-08 02:09:41 +0530

    Final update

--- tripplanner/src/components/CardPanel.js
+++ tripplanner/src/components/CardPanel.js
@@ -1,6 +1,5 @@
-import React, {useState, Suspense} from 'react';
+import React, {useState} from 'react';
 import {Card, Button, Row, Col, Input, Empty, Form} from 'antd';
-import {connect} from 'react-redux';
 import 'antd/dist/antd.css';
 import {
     AppstoreOutlined,
@@ -13,7 +12,7 @@ import {
 import '../css/style.css'
 import UpdateModel from "./updateModel";
 
-const CardPanel = ({transfer, onDelete, onUpdate,index}) => {
+const CardPanel = ({transfer, onDelete, onUpdate, index}) => {
     const [show, setShow] = useState(false)
     const [updateModalVisible, handleUpdateModalVisible] = useState(false);
 
@@ -25,143 +24,142 @@ const CardPanel = ({transfer, onDelete, onUpdate,index}) => {
     const {Adult, Children, dropLocation, firstName, id, lastName, pickUpLocation, vechileCount, vechileType, date, time, city} = transfer;
     return (
         <Row justify="center">
-                    <Col key={id}>
-                    <Card className="infoCard">
-                        <Row>
-                            <Col>
-                                <Card className='smallCard' bodyStyle={{padding:0}}>
-                                    <p className='date'>{date.slice(0,5)}</p>
-                                </Card>
-                            </Col>
-                            <Col>
-                                <p className="dayHead">Day {index}</p>
-                            </Col>
-
-                        </Row>
-                        <Row justify="center">
-                            <Card className="infoCard">
-                                <Row>
-                                    <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-                                        <p className='head'>
-                                            <AppstoreOutlined className='icon'/>
-                                            <CarOutlined className='icon'/>
-                                            Transfer Services
-                                        </p>
-                                    </Col>
-                                    <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-                                        <Row justify="end">
-                                            <Button className="btn"
-                                                    onClick={() => handleUpdateModalVisible(true)}><EditOutlined/></Button>
-                                            <Button className="btn" onClick={() => onDelete(id)}><DeleteOutlined/></Button>
-                                            <Button className="btn" onClick={Toggle}>
-                                                {
-                                                    show ? <CaretDownOutlined/> : <CaretUpOutlined/>
-                                                }
-                                            </Button>
-                                        </Row>
-                                    </Col>
-                                </Row>
-
-                                {
-                                    show && (
-                                        <div className="data">
-                                            <Row>
-                                                <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-                                                    <Row>
-                                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            <p className="heads">Pickup Location</p>
-                                                        </Col>
-
-                                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            <p>{pickUpLocation}</p>
-                                                        </Col>
-                                                    </Row>
-                                                </Col>
-                                                <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-                                                    <Row>
-                                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            <p className="heads">Service Type</p>
-                                                        </Col>
-
-                                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            <p>Airport Transfer</p>
-                                                        </Col>
-                                                    </Row>
-                                                </Col>
-                                            </Row>
-
-                                            <Row>
-                                                <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-                                                    <Row>
-                                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            <p className="heads">Drop Location</p>
-                                                        </Col>
-
-                                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            <p>{dropLocation}</p>
-                                                        </Col>
-                                                    </Row>
-                                                </Col>
-                                                <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-                                                    <Row>
-                                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            <p className="heads">Vechile Type</p>
-                                                        </Col>
-
-                                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            <p>{vechileType} * {vechileCount}</p>
-                                                        </Col>
-                                                    </Row>
-                                                </Col>
-                                            </Row>
-
-
-                                            <Row>
-                                                <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-                                                    <Row>
-                                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            <p className="heads">City</p>
-                                                        </Col>
-
-                                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            <p>{city}</p>
-                                                        </Col>
-                                                    </Row>
-                                                </Col>
-                                                <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-                                                    <Row>
-                                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            <p className="heads">Passengers Count</p>
-                                                        </Col>
-
-                                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            <p>{`${Adult} adults, ${Children} children`}</p>
-                                                        </Col>
-                                                    </Row>
-                                                </Col>
-                                            </Row>
-
-                                            <Row>
-                                                <Col xs={24} sm={24} md={12} lg={12} xl={12}>
-                                                    <Row>
-                                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            <p className="heads">Start Date Time</p>
-                                                        </Col>
-
-                                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                                            <p>{date} | {time}</p>
-                                                        </Col>
-                                                    </Row>
-                                                </Col>
-                                            </Row>
-                                        </div>
-                                    )
-                                }
+            <Col key={id}>
+                <Card className="infoCard">
+                    <Row>
+                        <Col>
+                            <Card className='smallCard' bodyStyle={{padding: 0}}>
+                                <p className='date'>{date.slice(0, 5)}</p>
                             </Card>
-                        </Row>
-                    </Card>
-                </Col>
+                        </Col>
+                        <Col>
+                            <p className="dayHead">Day {index}</p>
+                        </Col>
 
+                    </Row>
+                    <Row justify="center">
+                        <Card className="infoCard">
+                            <Row>
+                                <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                                    <p className='head'>
+                                        <AppstoreOutlined className='icon'/>
+                                        <CarOutlined className='icon'/>
+                                        Transfer Services
+                                    </p>
+                                </Col>
+                                <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                                    <Row justify="end">
+                                        <Button className="btn"
+                                                onClick={() => handleUpdateModalVisible(true)}><EditOutlined/></Button>
+                                        <Button className="btn" onClick={() => onDelete(id)}><DeleteOutlined/></Button>
+                                        <Button className="btn" onClick={Toggle}>
+                                            {
+                                                show ? <CaretDownOutlined/> : <CaretUpOutlined/>
+                                            }
+                                        </Button>
+                                    </Row>
+                                </Col>
+                            </Row>
+
+                            {
+                                show && (
+                                    <div className="data">
+                                        <Row>
+                                            <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                                                <Row>
+                                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                                        <p className="heads">Pickup Location</p>
+                                                    </Col>
+
+                                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                                        <p>{pickUpLocation}</p>
+                                                    </Col>
+                                                </Row>
+                                            </Col>
+                                            <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                                                <Row>
+                                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                                        <p className="heads">Service Type</p>
+                                                    </Col>
+
+                                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                                        <p>Airport Transfer</p>
+                                                    </Col>
+                                                </Row>
+                                            </Col>
+                                        </Row>
+
+                                        <Row>
+                                            <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                                                <Row>
+                                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                                        <p className="heads">Drop Location</p>
+                                                    </Col>
+
+                                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                                        <p>{dropLocation}</p>
+                                                    </Col>
+                                                </Row>
+                                            </Col>
+                                            <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                                                <Row>
+                                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                                        <p className="heads">Vechile Type</p>
+                                                    </Col>
+
+                                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                                        <p>{vechileType} * {vechileCount}</p>
+                                                    </Col>
+                                                </Row>
+                                            </Col>
+                                        </Row>
+
+
+                                        <Row>
+                                            <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                                                <Row>
+                                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                                        <p className="heads">City</p>
+                                                    </Col>
+
+                                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                                        <p>{city}</p>
+                                                    </Col>
+                                                </Row>
+                                            </Col>
+                                            <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                                                <Row>
+                                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                                        <p className="heads">Passengers Count</p>
+                                                    </Col>
+
+                                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                                        <p>{`${Adult} adults, ${Children} children`}</p>
+                                                    </Col>
+                                                </Row>
+                                            </Col>
+                                        </Row>
+
+                                        <Row>
+                                            <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                                                <Row>
+                                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                                        <p className="heads">Start Date Time</p>
+                                                    </Col>
+
+                                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                                        <p>{date} | {time}</p>
+                                                    </Col>
+                                                </Row>
+                                            </Col>
+                                        </Row>
+                                    </div>
+                                )
+                            }
+                        </Card>
+                    </Row>
+                </Card>
+            </Col>
 
             <UpdateModel
                 onSubmit={transfer => onUpdate(transfer)}
@@ -170,11 +168,9 @@ const CardPanel = ({transfer, onDelete, onUpdate,index}) => {
                 }}
                 updateModalVisible={updateModalVisible}
                 values={transfer}
-                handleUpdateModalVisible={handleUpdateModalVisible}
-            />
+                handleUpdateModalVisible={handleUpdateModalVisible}/>
         </Row>
     )
 }
-
 
 export default CardPanel;
